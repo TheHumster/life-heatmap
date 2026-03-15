09c29a3a73bf49fb9c0a07c10ba1bbaa37b574a6
Humster <hamzahmalik1@hotmail.com>
2026-03-15 11:30:33 +0000

    finalise postgres connection, add activity endpoint with stub data to populate db

--- main.go
+++ main.go
@@ -33,9 +33,26 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Life Heatmap server is running buddy!")
 }
 
+func addActivity(w http.ResponseWriter, r *http.Request) {
+	_, err := db.Exec(
+		context.Background(),
+		"INSERT INTO activities (name, minutes) VALUES ($1, $2)",
+		"coding",
+		60,
+	)
+
+	if err != nil {
+		http.Error(w, "Failed to add activity", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintln(w, "Activity added successfully")
+}
+
 func main() {
 	connectDB()
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/add", addActivity)
 
 	fmt.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
